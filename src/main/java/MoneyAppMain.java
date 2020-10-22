39793fa681916646ad1ac4de2a73c5fd6f73de01
Pavel Lissin <zvuk331@gmail.com>
2020-10-22 22:50:30 +0600

    Clear MoneyAppMain class. Test.

--- src/main/java/MoneyAppMain.java
+++ src/main/java/MoneyAppMain.java
@@ -1,42 +1,8 @@
-import Client.Client;
-import Server.Server;
 
 import java.io.IOException;
 
 public class MoneyAppMain {
     public static void main(String[] args) throws IOException {
-
-
-            /*String requestClient = "Client request";
-            client.writeLine(requestClient);
-            System.out.println(requestClient);
-
-            String requestServer = "Hello in my server";
-            server.writeLine(requestServer);
-            System.out.println(requestServer);
-
-            String responseServer = client.readLine();
-
-            String responseClient= server.readLine();
-
-            System.out.println(responseClient);
-
-            System.out.println(responseServer);*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
