6497d185037b3d209eba5709b6be0eecd1a728b8
Pavel Lissin <zvuk331@gmail.com>
2020-10-24 19:06:07 +0600

    Test SQL Connection

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,2 +1,39 @@
-package Database;public class Database {
+package Database;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Database {
+    private String url;
+    private String userName;
+    private String userPassword;
+    private static Database database;
+
+    private Database(){
+        try {
+            url = "jdbc:mysql://localhost:3306/mysql";
+            userName = "root@localhost";
+            userPassword = "pasha331799";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            try (Connection conn = DriverManager.getConnection(url,userName,userPassword))
+            {
+                Statement statement = conn.createStatement();
+                int rows = statement.executeUpdate("INSERT new_db.users(user_email, user_password) VALUES (\"testemail@gmail.com\", \"testpassword\")");
+                System.out.println(rows);
+            }
+        } catch (Exception e){
+            System.out.println("Connection error");
+            e.printStackTrace();
+        }
+
+    }
+    public static Database DATABASE_ON(){
+        if (database == null){
+            database = new Database();
+        }
+        return database;
+    }
 }
--- src/main/java/MoneyAppMain.java
+++ src/main/java/MoneyAppMain.java
@@ -1,8 +1,10 @@
 
+import Database.Database;
+
 import java.io.IOException;
 
 public class MoneyAppMain {
     public static void main(String[] args) throws IOException {
-
+        Database.DATABASE_ON();
     }
 }
