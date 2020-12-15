ca18876c178ddbf3ab144c084edbdaa15e8765e3
Pavel Lissin <zvuk331@gmail.com>
2020-12-16 00:13:32 +0600

    Attempts to add css

--- src/main/java/ru/java/learn/config/MvcConfig.java
+++ src/main/java/ru/java/learn/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package ru.java.learn.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,4 +13,10 @@ public class MvcConfig implements WebMvcConfigurer {
 
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
